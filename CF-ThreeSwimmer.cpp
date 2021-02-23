6c75fa12a8d201900d3e0f6d74045d29d0557005
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-02-23 20:40:45 +0530

    Add files via upload

---
+++ CF-ThreeSwimmer.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+ll min(int a,int b){
+    return (a<b)?a:b;
+}
+
+int main(){
+
+    ll t;
+    cin>>t;
+    while(t--){
+
+        ll n,a,b,c;
+        cin>>n>>a>>b>>c;
+        int min_time=INT16_MAX;
+
+      ll a1=(a-n%a)%a;
+     
+       ll  b1=(b-n%b)%b;
+    
+        min_time=min(a1,b1);
+     ll c1=(c-n%c)%c;
+ 
+        min_time=min(min_time,c1);
+        cout<<min_time<<"\n";
+    }
+
+
+}#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+ll min(int a,int b){
+    return (a<b)?a:b;
+}
+
+int main(){
+
+    ll t;
+    cin>>t;
+    while(t--){
+
+        ll n,a,b,c;
+        cin>>n>>a>>b>>c;
+        int min_time=INT16_MAX;
+
+      ll a1=(a-n%a)%a;
+     
+       ll  b1=(b-n%b)%b;
+    
+        min_time=min(a1,b1);
+     ll c1=(c-n%c)%c;
+ 
+        min_time=min(min_time,c1);
+        cout<<min_time<<"\n";
+    }
+
+
+}v#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+ll min(int a,int b){
+    return (a<b)?a:b;
+}
+
+int main(){
+
+    ll t;
+    cin>>t;
+    while(t--){
+
+        ll n,a,b,c;
+        cin>>n>>a>>b>>c;
+        int min_time=INT16_MAX;
+
+      ll a1=(a-n%a)%a;
+     
+       ll  b1=(b-n%b)%b;
+    
+        min_time=min(a1,b1);
+     ll c1=(c-n%c)%c;
+ 
+        min_time=min(min_time,c1);
+        cout<<min_time<<"\n";
+    }
+
+
+}#include<bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+ll min(int a,int b){
+    return (a<b)?a:b;
+}
+
+int main(){
+
+    ll t;
+    cin>>t;
+    while(t--){
+
+        ll n,a,b,c;
+        cin>>n>>a>>b>>c;
+        int min_time=INT16_MAX;
+
+      ll a1=(a-n%a)%a;
+     
+       ll  b1=(b-n%b)%b;
+    
+        min_time=min(a1,b1);
+     ll c1=(c-n%c)%c;
+ 
+        min_time=min(min_time,c1);
+        cout<<min_time<<"\n";
+    }
+
+
+}
