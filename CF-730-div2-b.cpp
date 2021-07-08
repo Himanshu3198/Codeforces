6e00983232f49a55afdbe1dac77936af0d25bfe1
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2021-07-08 14:07:50 +0530

    Add files via upload

---
+++ CF-730-div2-b.cpp
@@ -0,0 +1,65 @@
+/* 
+  author:himanshu3198
+  problem-codeforces 730 B. Customising the Track
+
+
+  */
+#include <bits/stdc++.h>
+#define ll long long int
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define FORR(i, n) for (int i = (n); i >= 0; i--)
+#define fst first
+#define snd second
+#define E "\n"
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007LL
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define setbits(x) __builtin_popcount(x)
+#define fastio()        ios_base::sync_with_stdio(false);  cin.tie(NULL);
+using namespace std;
+
+
+
+int main()
+{
+    fastio();
+    int t=1;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin>>n;
+        ll a[n];
+        ll sum=0;
+
+        fr(i,n){
+            cin>>a[i];
+            sum+=a[i];
+        }
+
+     
+
+        cout<<sum%n*(n-sum%n)<<E;
+
+      
+
+
+
+
+
+    
+
+
+       
+
+    
+       
+    }
+
+    return 0;
+}
