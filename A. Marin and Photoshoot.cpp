f76222a29ca4a7ad59ce656daf1a7cb869c12a71
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-03-27 22:40:59 +0530

    Create A. Marin and Photoshoot.cpp

---
+++ A. Marin and Photoshoot.cpp
@@ -0,0 +1,76 @@
+
+/*
+ author:himanshu3198
+https://codeforces.com/contest/1658/problem/A
+*/
+#include     <bits/stdc++.h>
+#define      ll long long int
+#define      fr(i, n) for (int i = 0; i < (n); i++)
+#define      rep(i, a, b) for (int i = (a); i < (b); i++)
+#define      FORR(i, n) for (int i = (n); i >= 0; i--)
+#define      fst first
+#define      snd second
+#define      E "\n"
+#define      pb push_backCF
+#define      mp make_pair
+#define      all(x) (x).begin(), (x).end()
+#define      MOD 1000000007LL
+#define      MAX(a, b) ((a) > (b) ? (a) : (b))
+#define      MIN(a, b) ((a) < (b) ? (a) : (b))
+#define      ABS(x) ((x) < 0 ? -(x) : (x))
+#define      setbits(x) __builtin_popcount(x)
+#define      vi vector<int>
+#define      vl vector<long long int>
+#define      vvi vector<vector<int>>
+#define      vvl vector<vector<long long int>>
+#define      INF 1e18
+#define      _Y puts("Yes")
+#define      _N puts("No")
+#define      showArr(v) for(auto it:v) cout<<it<<" "; cout<<E;
+#define      Print(ans) cout<<ans<<E;
+#define      Nitro()                      \
+             ios_base::sync_with_stdio(false); \
+             cin.tie(NULL);
+
+using namespace std;
+
+
+void himanshu3198()
+{
+     
+     int n;
+     cin>>n;
+     string s;
+     cin>>s;
+ 
+     int res=0;
+
+    for(int i=0;i<n-1;i++){
+
+        if(i-1>=0 and i+1<n and s[i]=='1' and s[i-1]=='0' and s[i+1]=='0'){
+            res++;
+        }
+        if(i+1<n and s[i]=='0' and s[i+1]=='0'){
+              res+=2;
+        }
+    }
+
+    cout<<res<<E;
+    
+}
+
+
+int main()
+{
+    Nitro();
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+
+        himanshu3198();
+    }
+
+    return 0;
+}
