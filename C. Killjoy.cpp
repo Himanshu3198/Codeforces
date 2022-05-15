bf82fba69be8a36448c48b9d1b927471b1cd683c
HIMANSHU SHARMA <54854216+Himanshu3198@users.noreply.github.com>
2022-05-15 15:29:45 +0530

    Create C. Killjoy.cpp

---
+++ C. Killjoy.cpp
@@ -0,0 +1,89 @@
+
+/*
+ author:himanshu3198
+
+*/
+#include <bits/stdc++.h>
+#define ll long long int
+#define fr(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define FORR(i, n) for (int i = (n); i >= 0; i--)
+#define fst first
+#define snd second
+#define E "\n"
+#define pb push_back
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define MOD 1000000007LL
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define setbits(x) __builtin_popcount(x)
+#define vi vector<int>
+#define vl vector<long long int>
+#define vvi vector<vector<int>>
+#define vvl vector<vector<long long int>>
+#define INF 1e18
+#define _Y puts("YES")
+#define _N puts("NO")
+#define showArr(v)         \
+    for (auto it : v)      \
+        cout << it << " "; \
+    cout << E;
+#define Print(ans) cout << ans << E;
+#define sz(s) s.size();
+#define Nitro()                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+using namespace std;
+
+
+
+void solve()
+{
+     ll n,x;
+     cin>>n>>x;
+     vl a(n);
+     fr(i,n) cin>>a[i];
+    //  sort(all(a));
+     bool flag=false;
+    
+     ll res=0,sum=0,cnt=0;
+     fr(i,n){
+        if(a[i]==x){
+            flag=true;
+            cnt++;
+        
+        } 
+          sum+=(a[i]-x);
+        
+     }
+    
+    
+     if(cnt==n){
+         Print(0);
+         
+     }else{
+         if(flag or sum==0){
+             Print(1);
+         }else{
+             Print(2);
+         }
+     }
+}
+
+
+int main()
+{
+    Nitro();
+    int t = 1;
+    cin >> t;
+
+    while (t--)
+    {
+
+        solve();
+    }
+
+    return 0;
+}
